77fd4602b5d4c3b685b68773a034e24df4399f94
B0ba26 <boba135t24@gmail.com>
2024-12-18 14:45:44 -0800

    Add files via upload

---
+++ activity 3.py
@@ -0,0 +1,42 @@
+#Write a program to find what type of datatype the given variable is?
+
+num=52
+print(type(num))
+name='Arya'
+print (type(name))
+number=0.1
+print (type(number))
+text=True
+print (type(text))
+#Write a program to change the datatype of given variables?
+#Typecasting is converting one datatype to another.
+
+
+aoifjsai=7
+print ("the original data type is :",type(aoifjsai))
+
+
+aoifjsai=float(aoifjsai)
+print("the converted data type is :",type(aoifjsai))
+
+fjahjsfo=7
+print("the original datatype is :",type(fjahjsfo))
+fjahjsfo=str(fjahjsfo)
+print ("the converted data type is:",type(fjahjsfo))
+
+###String Operations
+
+
+y="coding"
+print(len(y))
+print(y[5])
+
+print(y[:3])
+print(y[3:])
+name='Arya'
+print(name[::-1])
+abc='gfaubsdhiklfulvjcnsudikjhkfa'
+print(abc[::-1])
+coding='nohtyp'
+print(coding[::-1])
+
